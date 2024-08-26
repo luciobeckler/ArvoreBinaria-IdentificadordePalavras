0c3df1e04f604a94d3d031aa842f748791cd2cd8
Lúcio Beckler <luciobecklerp@gmail.com>
2024-08-26 09:56:07 -0300

    Frist commit

---
+++ src/Arvore/Arvore.java
@@ -0,0 +1,29 @@
+package Arvore;
+
+public class Arvore {
+  Celula raiz;
+
+  private Celula inserirNovo(Celula novo, Celula atual){
+
+  }
+
+  void Inserir(Celula novo){
+
+  }
+
+  private void PreOrdem(Celula elemento){
+
+  }
+
+  private void PosOrdem(Celula elemento){
+
+  }
+
+  private void EmOrdem(Celula elemento){
+
+  }
+
+  public PreOrdem(){
+    
+  }
+}
---
+++ src/Arvore/Celula.java
@@ -0,0 +1,7 @@
+package Arvore;
+
+public class Celula {
+  int valor;
+  Celula direita;
+  Celula esquerda;
+}
